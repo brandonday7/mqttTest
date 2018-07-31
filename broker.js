7d0299e6d53d222fd3ffe02e88cc25161fca87c3
Brandon Day <brandontday7@gmail.com>
2018-07-31 11:50:24 -0400

    console logs to find why crashing

--- broker.js
+++ broker.js
@@ -1,11 +1,13 @@
 var mosca = require('mosca');
 
 const PORT = process.env.PORT
+console.log(`we have a port number ${PORT}`)
 var settings = {
   port : PORT || 3000
 }
 
 var server = new mosca.Server(settings);
+console.log("server var established")
 
 server.on('ready', function(){
   console.log(`MQTT Broker running on Port: ${PORT}`)
@@ -13,5 +15,6 @@ server.on('ready', function(){
 
 
 server.on('published', function(packet, client) {
+  console.log("made it this far")
   console.log('Published', packet.payload);
 });
